2f6a8f2f889f5a3e49c6d5f8665548310e0aa8c8
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-07 11:47:41 +0600

    add daisyui

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log('click')}>Add click</button>
+      <button className="btn btn-primary" onClick={() => console.log('click')}>
+        Add click
+      </button>
     </div>
   );
 };
