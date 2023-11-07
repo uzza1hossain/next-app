5b89c54684fe79ec59d1f9ef0b82163d4e72bd90
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-07 10:54:24 +0600

    catch with revalidation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,10 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    // cache: "no-cache",
+    next: { revalidate: 10 },
+  });
   const users: User[] = await res.json();
   return (
     <>
