6954cfa6de7d3b4ae0bfe227b93be6b5516d6393
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-07 11:02:04 +0600

    ssr with dynamic reandring

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,14 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    // cache: "no-cache",
-    next: { revalidate: 10 },
+    cache: 'no-store',
+    // next: { revalidate: 10 },
   });
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}> {user.name}</li>
