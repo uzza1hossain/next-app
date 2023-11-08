0bfb16b7fcd387fe8bd9888d92fdc5c15f4f7afc
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-08 20:01:58 +0600

    add prisma CURD

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,22 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
 import schema from './schema';
+import prisma from '@/prisma/client';
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: 'Mosh' },
-    { id: 2, name: 'John' },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
-  if (!validation.success) return NextResponse.json(validation.error.errors,{status:400});
-  return NextResponse.json(
-    {
-      id: Math.round(Math.random() * 100),
-      name: body.name,
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const user = await prisma.user.findUnique({
+    where: { email: validation.data.email },
+  });
+  if(user)return NextResponse.json({error: "User already exists"}, { status: 400 });
+  const newUser = await prisma.user.create({
+    data: {
+      name: validation.data.name,
+      email: validation.data.email,
     },
-    { status: 201 }
-  );
+  });
+  return NextResponse.json(newUser, { status: 201 });
 }
