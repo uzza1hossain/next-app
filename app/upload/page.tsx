c7f3eab4b96afea95beff4a2f6de29d96eabf41e
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-09 12:02:36 +0600

    add option to display uploaded image

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,15 +1,32 @@
 'use client';
-import React from 'react';
-import { CldUploadWidget } from 'next-cloudinary';
+import React, { useState } from 'react';
+import { CldImage, CldUploadWidget } from 'next-cloudinary';
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState('');
   return (
-    <CldUploadWidget uploadPreset="ih2wlf8w">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} alt="Coffee" width={270} height={180} />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="ih2wlf8w"
+        onUpload={(result, widget) => {
+          if (result.event !== 'success') return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}>
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
