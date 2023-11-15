ea425ff5b53a7914b5078b0a4615afd35db556d0
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-15 23:17:31 +0600

    add style to the email

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -6,20 +6,31 @@ import {
   Text,
   Link,
   Preview,
+  Tailwind
 } from '@react-email/components';
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
+      <Tailwind>
+        
+      <Body className='bg-white'>
         <Container>
-          <Text>Hello {name}</Text>
+          <Text className='font-bold text-3xl'>Hello {name}</Text>
           <Link href="https://www.codewithmosh.com">www.codewithmosh.com</Link>
         </Container>
       </Body>
+      </Tailwind>
     </Html>
   );
 };
 
+const body: React.CSSProperties = {
+  background: '#fff'
+}
+
+const heading: React.CSSProperties = {
+  fontSize: '32px'
+}
 export default WelcomeTemplate;
