2ba5a042d436b2b38328486ea17a8af61c3f02e9
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-26 21:35:25 +0600

    add image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import Link from 'next/link';
-import ProductCard from './components/ProductCard';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
-
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+import Image from 'next/image';
+import coffee from '@/public/images/coffee.jpg';
+async function Home() {
   return (
-    <main>
-      <h1>Hello World {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      {/* <Image src={coffee} alt="Coffee" /> */}
+      <Image
+        src="https://images.unsplash.com/photo-1497935586351-b67a49e012bf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Coffee"
+        fill
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+        className="object-cover"
+      />
     </main>
   );
 }
+
+export default Home;
